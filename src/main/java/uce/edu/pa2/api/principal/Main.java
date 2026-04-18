05baec412d0e9475e1fcd5e1ac3d64e6f65d9c85
a <caizaa07@gmail.com>
2026-04-17 21:53:59 -0500

    Deber 1

--- src/main/java/uce/edu/pa2/api/principal/Main.java
+++ src/main/java/uce/edu/pa2/api/principal/Main.java
@@ -25,7 +25,7 @@ public class Main {
             System.out.println("-----Impresión para pedidos-----");
             Pedido pedido1 = new Pedido("Alex Caiza","Vitamina C",20,"ales@gmail.com");
             this.service.crear(pedido1);  
-            System.out.println("-----Impresión para citas-----");
+            System.out.println("-----Impresión para citas----");
             Cita cita1 = new Cita("Alan Brito","Dr.Manuel Bravo","Cardiología","2024-07-15","alan@gmail.com");
             this.citaService.agendar(cita1);
 
