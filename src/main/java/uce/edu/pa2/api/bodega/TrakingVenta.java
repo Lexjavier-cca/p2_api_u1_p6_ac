d88f495cfb265a9fb8ba9a004adc8c75043d8936
LeonardoDavidRY <ldramirez@uce.edu.ec>
2026-04-24 18:11:56 -0500

    Probando el push

---
+++ src/main/java/uce/edu/pa2/api/bodega/TrakingVenta.java
@@ -0,0 +1,22 @@
+package uce.edu.pa2.api.bodega;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Dependent;
+// Esta clase se encarga de medir el tiempo de ejecución de una venta
+@Dependent
+public class TrakingVenta {
+    private long tiempoInicio = 0;
+    private long tiempoFinal = 0;
+
+    public void iniciar() {
+        this.tiempoInicio =this.tiempoInicio + System.currentTimeMillis();
+
+    }
+
+    public void finalizar(){
+        this.tiempoFinal =  this.tiempoFinal + System.currentTimeMillis();
+        long tiempoEjecucion = tiempoFinal - this.tiempoInicio;
+        System.out.println("Tiempo de ejecución: " + tiempoEjecucion + " ms");
+    }
+
+}
