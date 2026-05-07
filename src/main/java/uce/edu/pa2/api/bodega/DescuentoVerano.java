796d0ea2bee3f7567d1566646241a819a496988e
Alex Caiza <caizaa07@gmail.com>
2026-05-06 19:27:15 -0500

    Alex Caiza Taller12: Priority en extensiones

--- src/main/java/uce/edu/pa2/api/bodega/DescuentoVerano.java
+++ src/main/java/uce/edu/pa2/api/bodega/DescuentoVerano.java
@@ -1,8 +1,11 @@
 package uce.edu.pa2.api.bodega;
 
+
+import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
+@Priority(4)
 public class DescuentoVerano implements Descuento {
     @Override
     public double aplicar(double valor){
